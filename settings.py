15d9decf7d76fc8de363429f6dbe0f8c594a72d4
Evaristo R Rivieccio Vega <evtrompa@gmail.com>
2019-02-08 20:36:32 +0000

    con ldap

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import ldap
+from django_auth_ldap.config import LDAPSearch, GroupOfNamesType
 
 INSTALLED_ADDONS = [
     # <INSTALLED_ADDONS>  # Warning: text inside the INSTALLED_ADDONS tags is auto-generated. Manual changes will be overwritten.
@@ -18,6 +20,7 @@ INSTALLED_ADDONS = [
     # </INSTALLED_ADDONS>
 ]
 
+
 import aldryn_addons.settings
 aldryn_addons.settings.load(locals())
 
@@ -26,4 +29,26 @@ aldryn_addons.settings.load(locals())
 
 INSTALLED_APPS.extend([
     # add your project specific apps here
+     'django_extensions',
 ])
+
+
+
+AUTHENTICATION_BACKENDS = [
+    'django_auth_ldap.backend.LDAPBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+AUTH_LDAP_SERVER_URI = "ldap://192.168.1.70"
+
+AUTH_LDAP_BIND_DN = "cn=admin,dc=planetexpress,dc=com"
+AUTH_LDAP_BIND_PASSWORD = "GoodNewsEveryone"
+AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=people,dc=planetexpress,dc=com",
+    ldap.SCOPE_SUBTREE, "(cn=%(user)s)")
+
+
+AUTH_LDAP_GROUP_SEARCH = LDAPSearch("ou=people,dc=planetexpress,dc=com",
+    ldap.SCOPE_SUBTREE, "(objectClass=Group)"
+)
+AUTH_LDAP_GROUP_TYPE = GroupOfNamesType()
+AUTH_LDAP_MIRROR_GROUPS = True
